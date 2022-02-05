fdbf2221b6042dd65cc323e45f772e43c0be06dc
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2022-02-05 15:36:07 +0100

    Adding ingredients from a list fetched from the db
    Issues: Values of list items only updates in the dom when adding a new item to the array

--- hanterare/src/components/createRecipe.js
+++ hanterare/src/components/createRecipe.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import useFetch from './useFetch';
 
 const CreateRecipe = () => {
 
   const [receptNamn, setReceptNamn] = useState('');
   const [beskrivning, setBeskrivning] = useState('');
-  const [ingrediens, setIngrediens] = useState('hej');
+  const [ingredienser, setIngredienser] = useState([]);
   const [bild, setBild] = useState();
 
   const handleSubmit = (e) => {
@@ -15,48 +16,81 @@ const CreateRecipe = () => {
     }
   }
 
-  return(
+  const handleAddIngredient = () => {
+    setIngredienser([...ingredienser, ""])
+  }
+
+  const handleChangeIngredient = (e, index) => {
+
+    const tempArr = ingredienser;
+    tempArr[index] = e.target.value;
+
+    setIngredienser(tempArr);
+  }
+
+  const { data: ingrediens, isPending, error } = useFetch('http://192.168.0.122:8000/allaIngredienser', 'GET');
+
+  return (
     <div className="create">
       <h2>Lägg till ett nytt recept</h2>
       <form onSubmit={handleSubmit}>
+
         <label>Receptnamn</label>
-        <input 
+        <input
           type="text"
           required
           value={receptNamn}
           onChange={(e) => setReceptNamn(e.target.value)}
-          />
+        />
 
         <label>Beskrivning</label>
-        <textarea 
+        <textarea
           required
           value={beskrivning}
           onChange={(e) => setBeskrivning(e.target.value)}
-          />
+        />
+        {ingrediens && <p>{ingrediens.namn}</p>}
+        {error && <p>{error}</p>}
+        {isPending && <p>{isPending}</p>}
+        <div className="ingrediensRubrik">
+          <p>Ingrediens</p>
+          <p>Mängd</p>
+          <p>Enhet</p>
+        </div>
+        {ingrediens && <>
+          {ingredienser.map((item, index) => {
+            return (<div key={index}>
+              <p>{index}</p>
+              <label>Lägg till ingredienser</label>
+              <select
+                value={item}
+                onChange={(e) => handleChangeIngredient(e, index)}
+              >
+                {ingrediens.map((ingred, index) => {
+                  return(<option value={ingred.namn} key={index}>{ingred.namn}</option>)
+                })}
+              </select>
+            </div>)
+          })}
 
-          <label>Ingrediens</label>
-          <select
-            value={ ingrediens }
-            onChange={(e) => setIngrediens(e.target.value)}
-          >
-            <option value="hej"></option>
-            <option value="då"></option>
-          </select>
+        </>}
 
-          <label>Bild</label>
-          <input 
-            type="file" 
-            id="bild" 
-            name="filename"
-            onChange={setBild}
-          />
-          <label htmlFor="altText">Alt attribut</label>
-          <input 
-            type="text"
-            id="altText"
-          />
+        <input type="button" value="+" onClick={() => handleAddIngredient()} />
+
+        <label>Bild</label>
+        <input
+          type="file"
+          id="bild"
+          name="filename"
+          onChange={setBild}
+        />
+        <label htmlFor="altText">Alt attribut</label>
+        <input
+          type="text"
+          id="altText"
+        />
       </form>
-      <p> { receptNamn }</p>
+      <p> {receptNamn}</p>
     </div>
   );
 }
