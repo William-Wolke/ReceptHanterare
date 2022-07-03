1283f866cdd0940eeba3c91518c34aa4c73ae163
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2022-07-03 16:07:39 +0200

    Styled recipelist

--- hanterare/src/components/RecipeList.jsx
+++ hanterare/src/components/RecipeList.jsx
@@ -7,32 +7,33 @@ const RecipeList = () => {
     const { data: recipe, isPending, error } = useFetch("/recipe/all/", 'GET');
 
     return (
-        <div className="ReceptLista" key="recipeListMain">
-            <div>
-              
-            </div>
+        <div className="recipeList" key="recipeListMain">
+
             {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
             {recipe && recipe.map((recipe, index) => {
                 return (
                     <Link to={"/recept/" + recipe.name} key={"recipe" + index}>
-                        <div key={recipe.name + "container" + index}>
-                            {/*<div className="receptBild" key={recipe.name+ " bilddiv"}>
-                                <img src={recipe.image} alt={recipe.bildtext} key={recipe.name + " bild"}/>
-                            </div>*/}
-                            <div className="receptNamn" key={recipe.name + " attribut"}>
-                                <p key={recipe.name + " tid"}>{recipe.attribute.time ? recipe.attribute.time : 0} min</p>
-                            </div>
-                            <div className="receptNamn" key={recipe.name + " namndiv"}>
-                                <h3 key={recipe.name + " name"}>{recipe.name}</h3>
-                            </div>
-                            <div>
-                                <img src={new URL("/static/" + recipe.image, process.env.REACT_APP_DB_HOSTNAME).href} alt={recipe.alt} height="100" width="100" />
-                            </div>
-                            <div key={recipe.name + " buttondiv"}>
-                                <button key={recipe.name + " button"}>
-                                    <i key={recipe.name + "icon"}></i>
-                                </button>
+                        <div className='recipeListItemContainer card'>
+                            <div className="recipeListItem" key={recipe.name + "container" + index}>
+                                <div className="recipeListTime" key={recipe.name + " attribut"}>
+                                    <p key={recipe.name + " tid"}>{recipe.attribute.time ? recipe.attribute.time : 0} min</p>
+                                </div>
+                                <div className="recipeListName" key={recipe.name + " namndiv"}>
+                                    <h3 key={recipe.name + " name"}>{recipe.name}</h3>
+                                </div>
+                                <div>
+                                    <img 
+                                        src={new URL("/static/" + recipe.image, process.env.REACT_APP_DB_HOSTNAME).href} 
+                                        alt={recipe.alt}
+                                        className="recipeListThumbnail" 
+                                    />
+                                </div>
+                                <div key={recipe.name + " buttondiv"}>
+                                    <button  className="recipeListButton" key={recipe.name + " button"}>
+                                        <p>Lägg till i veckomeny</p>
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </Link>
