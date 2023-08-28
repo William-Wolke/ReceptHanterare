c8b118bf112d4dac98d59da2e76dad1a6c318c28
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2023-08-28 20:13:28 +0200

    rename UseAxios since it wasn't a hook

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const UseAxios = async (path, body) => {
+export default async function apiPost(path, body) {
     let response = false;
 
     try {
-        let url = new URL(path, process.env.REACT_APP_DB_HOSTNAME).href;
+        let url = new URL(path, process.env.NEXT_PUBLIC_BASE_URL).href;
         let res = await axios.post(url, body);
         if (res.status === 200) {
             response = true;
@@ -15,5 +15,3 @@ const UseAxios = async (path, body) => {
         response = false;
     }
 };
-
-export default UseAxios;
