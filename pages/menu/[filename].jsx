09c2427bcc8aa7d57e2c8a86d4c95382f81fef40
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2023-09-29 21:37:40 +0200

    add basic menu ingredient style

--- pages/menu/[filename].jsx
+++ pages/menu/[filename].jsx
@@ -1,17 +1,17 @@
 import Link from 'next/link';
-import client from '../../tina/__generated__/client'
+import client from '../../tina/__generated__/client';
 import { getShoppingList, summarizeShoppingList } from '../../src/helpers';
 import { TinaMarkdown } from 'tinacms/dist/rich-text';
 
 export const getStaticProps = async ({ params }) => {
-    let data = {}
-    let query = {}
-    let variables = { relativePath: `${params.filename}.yaml` }
+    let data = {};
+    let query = {};
+    let variables = { relativePath: `${params.filename}.yaml` };
     try {
-        const res = await client.queries.menus(variables)
-        query = res.query
-        data = res.data
-        variables = res.variables
+        const res = await client.queries.menus(variables);
+        query = res.query;
+        data = res.data;
+        variables = res.variables;
     } catch {
         // swallow errors related to document creation
     }
@@ -23,28 +23,24 @@ export const getStaticProps = async ({ params }) => {
             query: query,
             //myOtherProp: 'some-other-data',
         },
-    }
-}
+    };
+};
 export const getStaticPaths = async () => {
-    const menusListData = await client.queries.menusConnection()
+    const menusListData = await client.queries.menusConnection();
     return {
         paths: menusListData.data.menusConnection.edges.map((post) => ({
             params: { filename: post.node._sys.filename },
         })),
         fallback: false,
-    }
-}
+    };
+};
 
 export default function Menu({ data }) {
     const menu = data.menus;
-    let shoppingList = [];
-    if (menu?.recipes) {
-        shoppingList = summarizeShoppingList(getShoppingList(menu.recipes));
-    }
+    const shoppingList = summarizeShoppingList(getShoppingList(menu.recipes));
 
     const weekday_recipes = {};
     if (menu?.recipes) {
-
         for (const recipe of menu.recipes) {
             if (recipe.day in weekday_recipes) {
                 weekday_recipes[recipe.day].push(recipe.name);
@@ -60,65 +56,77 @@ export default function Menu({ data }) {
                 <div>
                     <div className="">
                         {/* Titel */}
-                        <div className='w-5/6 mx-auto'>
+                        <div className="w-5/6 mx-auto">
                             <Link href={`/menu/${menu._sys.filename}`}>
-                                <h3 className='text-xl text-center'>
-                                    {menu.title}
-                                </h3>
+                                <h3 className="text-xl text-center">{menu.title}</h3>
                             </Link>
                         </div>
 
                         <div className="flex flex-row gap-6 mt-6 flex-wrap w-5/6 mx-auto">
-                            {weekday_recipes && Object.entries(weekday_recipes).reverse().map(([day, recipes], i) => {
-                                if (recipes.length == 0) {
-                                    return;
-                                }
-                                return (
-                                    <div key={day + i} className="h-56 w-64">
-                                        {/* TODO: Make this a reusable component*/}
-                                        {recipes && recipes.map((recipe, index) => {
-                                            return (
-                                                <div>
-                                                    <div className='h-36 w-42 rounded-t relative bg-[#84b082]'>
-                                                        <span className='absolute top-0 left-0 bg-gray-600 text-white opacity-70 px-2 py-1 rounded-tl'>{day}</span>
-                                                        {recipe.image && <Image src={recipe.image} height="200" width="200" className="" alt={recipe.title} />}
-                                                    </div>
-                                                    <div className="" key={`weekdayrecipe${index}`}>
-                                                        <div className='flex flex-row justify-between'>
-                                                            <p className='uppercase text-gray-600'>{recipe.time}</p>
-                                                            <p className='uppercase text-gray-600'>{recipe.servings} port</p>
-                                                        </div>
-                                                        <Link href={`/recipes/${recipe._sys.filename}`}>
-                                                            <h3 className='text-base text-gray-700 font-semibold'>{recipe.title}</h3>
-                                                        </Link>
-                                                    </div>
-                                                </div>
-                                            );
-                                        })}
-                                    </div>
-                                );
-                            })}
+                            {weekday_recipes &&
+                                Object.entries(weekday_recipes)
+                                    .reverse()
+                                    .map(([day, recipes], i) => {
+                                        if (recipes.length == 0) {
+                                            return;
+                                        }
+                                        return (
+                                            <div key={day + i} className="h-56 w-64">
+                                                {/* TODO: Make this a reusable component*/}
+                                                {recipes &&
+                                                    recipes.map((recipe, index) => {
+                                                        return (
+                                                            <div key={`${day}recipe${index}`}>
+                                                                <div className="h-36 w-42 rounded-t relative bg-[#84b082]">
+                                                                    <span className="absolute top-0 left-0 bg-gray-600 text-white opacity-70 px-2 py-1 rounded-tl">
+                                                                        {day}
+                                                                    </span>
+                                                                    {recipe.image && (
+                                                                        <Image
+                                                                            src={recipe.image}
+                                                                            height="200"
+                                                                            width="200"
+                                                                            className=""
+                                                                            alt={recipe.title}
+                                                                        />
+                                                                    )}
+                                                                </div>
+                                                                <div className="" key={`weekdayrecipe${index}`}>
+                                                                    <div className="flex flex-row justify-between">
+                                                                        <p className="uppercase text-gray-600">{recipe.time}</p>
+                                                                        <p className="uppercase text-gray-600">{recipe.servings} port</p>
+                                                                    </div>
+                                                                    <h3 className="text-base text-gray-700 font-semibold">
+                                                                        {recipe.title}
+                                                                    </h3>
+                                                                </div>
+                                                            </div>
+                                                        );
+                                                    })}
+                                            </div>
+                                        );
+                                    })}
                         </div>
                     </div>
-                    <div className="menuListItemSection">
-                        <div>
-                            <h2>Inköpslista</h2>
+                    <div className="w-3/4 mx-auto flex flex-col gap-4">
+                        <h3 className="text-xl">Inköpslista</h3>
+                        <div className="border border-gray-300 rounded-lg text-base">
+                            {shoppingList.length > 0 &&
+                                shoppingList.map((ingredient, index) => {
+                                    return (
+                                        <div
+                                            className="w-full flex flex-row border-b border-gray-300 py-2 px-3 gap-2"
+                                            key={'ingredient' + index}
+                                        >
+                                            <p className="font-medium">{ingredient.name}</p>
+                                            <p className="ml-auto">{ingredient.amount}</p>
+                                            <p>{ingredient.unit}</p>
+                                        </div>
+                                    );
+                                })}
                         </div>
-
-                        {shoppingList &&
-                            shoppingList.map((shoppingItem, index) => {
-                                return (
-                                    <div key={shoppingItem.name + index} className="menuListIngredientList">
-                                        <p>{shoppingItem.name}</p>
-
-                                        <p>{shoppingItem.amount}</p>
-
-                                        <p>{shoppingItem.unit}</p>
-                                    </div>
-                                );
-                            })}
                     </div>
-                </div >
+                </div>
             )}
         </div>
     );
