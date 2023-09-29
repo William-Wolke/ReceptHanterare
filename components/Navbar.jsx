16058017ac1bca6b775c760b8c151a946041b3df
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2023-09-29 21:58:23 +0200

    Improve styles (#24)
    * add tailwind styles to navbar
    
    * remove theme for now
    
    * add some tailwind styles to recipe
    
    * add tailwind styles to all pages
    
    * add servings to menu recipes
    
    * add basic menu ingredient style
    
    * add missing import
    
    * add missing import, key
    
    * add another missing key

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,22 +2,18 @@ import Link from 'next/link';
 
 export default function Navbar() {
     return (
-        <div className="navbarContainer">
-            <div className="navbar">
-                <div className="navbarLinkContainer">
-                    <Link href="/" className="navbarLink">
-                        <h1 className="navbarHeader">Receptsida</h1>
-                    </Link>
+        <div className="bg-[#84b082] flex flex-row gap-10 align-center h-20 pl-10">
+            <Link href="/" className="my-auto">
+                <h1 className="navbarHeader">Receptsida</h1>
+            </Link>
 
-                    <Link href="/recipes" className="navbarLink">
-                        <h2 className="navbarLinkText">Recept</h2>
-                    </Link>
+            <Link href="/recipes" className="my-auto">
+                <h2 className="text-lg">Recept</h2>
+            </Link>
 
-                    <Link href="/menu" className="navbarLink">
-                        <h2 className="navbarLinkText">Veckomenyer</h2>
-                    </Link>
-                </div>
-            </div>
+            <Link href="/menu" className="my-auto">
+                <h2 className="text-lg">Veckomenyer</h2>
+            </Link>
         </div>
     );
 }
